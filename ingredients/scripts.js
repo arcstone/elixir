c4db48961e96388eeff75d6c4b9a12b5b7c3a4a1
Ryan Hayle <rhayle@arcstone.com>
2015-06-02 22:23:01 -0500

    Added scriptOptions parameter to scripts and scriptsIn ingredients
    Adding {compileEs6: true} as the last parameter to .scripts() or
    .scriptsIn() will selectively enable compiling ES6 script with Babel on a
    case-by-case basis.

--- ingredients/scripts.js
+++ ingredients/scripts.js
@@ -13,23 +13,24 @@ var MergeRequest = require('./commands/MergeRequest');
  |
  */
 
-elixir.extend('scripts', function(scripts, outputDir, baseDir) {
+elixir.extend('scripts', function(scripts, outputDir, baseDir, scriptOptions) {
     outputDir = outputDir || elixir.config.jsOutput;
 
-    return combine(mergeRequest(scripts, outputDir, baseDir));
+    return combine(mergeRequest(scripts, outputDir, baseDir, scriptOptions));
 });
 
-elixir.extend('scriptsIn', function(baseDir, outputDir) {
+elixir.extend('scriptsIn', function(baseDir, outputDir, scriptOptions) {
     outputDir = outputDir || baseDir;
 
-    return combine(mergeRequest('**/*.js', outputDir, baseDir));
+    return combine(mergeRequest('**/*.js', outputDir, baseDir, scriptOptions));
 });
 
-var mergeRequest = function(scripts, outputDir, baseDir) {
+var mergeRequest = function(scripts, outputDir, baseDir, scriptOptions) {
     var request = new MergeRequest(scripts, baseDir, outputDir, 'js');
 
     request.taskName = 'scripts';
     request.minifier = require('gulp-uglify');
+    request.scriptOptions = scriptOptions || {};
 
     return request;
 };
